5a8518f305542df458e81b9872123167d67caaa4
Victoria <vmcnorrill@gmail.com>
2023-04-01 17:25:32 -0400

    removed colors.js

--- index.js
+++ index.js
@@ -1,58 +1,57 @@
 // linking files system, inquirer, lib/
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { colors } = require('./lib/colors.js');
 const { generateSvg } = require('./lib/generateSvg.js');
 const { Triangle, Circle, Square } = require('./lib/shapes.js');
 
 // function to initialize app
 function init() {
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Please enter the text for the logo (Maximum 3 characters):',
-        },
-        {
-            type: 'input',
-            name: 'textcolor',
-            message: 'Please enter a color for the text:',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape for the logo:',
-            choices: ['Triangle', 'Square', 'Circle'],
-        },
-        {
-            type: 'input',
-            name: 'shapecolor',
-            message: 'Please enter a color for the shape:',
-        },
-    ])
-    .then((response) => {
-        const logoPath = './dist/logo.svg';
-        let shape;
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Please enter the text for the logo (Maximum 3 characters):',
+            },
+            {
+                type: 'input',
+                name: 'textcolor',
+                message: 'Please enter a color for the text:',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose a shape for the logo:',
+                choices: ['Triangle', 'Square', 'Circle'],
+            },
+            {
+                type: 'input',
+                name: 'shapecolor',
+                message: 'Please enter a color for the shape:',
+            },
+        ])
+        .then((response) => {
+            const logoPath = './dist/logo.svg';
+            let shape;
 
-        if(response.shape === 'Triangle') {
-            shape = new Triangle();
-        } else if(response.shape === 'Square') {
-            shape = new Square();
-        } else {
-            shape = new Circle();
-        }
+            if (response.shape === 'Triangle') {
+                shape = new Triangle();
+            } else if (response.shape === 'Square') {
+                shape = new Square();
+            } else {
+                shape = new Circle();
+            }
 
-        shape.set(response.shapecolor)
+            shape.set(response.shapecolor)
 
-        fs.writeFile(logoPath, generateSvg(shape, response.textcolor, response.text), (err) =>
-        err ? console.error(err) : console.log('Successfully generated SVG logo.')
-        );
-    })
-    .catch((err) => {
-        console.log('Oops. Something went wrong.');
-        console.info(err)
-    });
+            fs.writeFile(logoPath, generateSvg(shape, response.textcolor, response.text), (err) =>
+                err ? console.error(err) : console.log('Successfully generated SVG logo.')
+            );
+        })
+        .catch((err) => {
+            console.log('Oops. Something went wrong.');
+            console.info(err)
+        });
 }
 
 // Function call to initialize app
