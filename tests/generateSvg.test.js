9b023ef77c1348cef7b5f4071cc95857eb6bf3ba
Victoria <vmcnorrill@gmail.com>
2023-04-09 14:22:06 -0400

    generateSvg.test.js not passing

--- tests/generateSvg.test.js
+++ tests/generateSvg.test.js
@@ -1,28 +1,15 @@
-const Logo = require('../lib/generateSvg')
+const generateSvg = require('../lib/generateSvg')
 
-describe('logo', () => {
+describe('generateSvg', () => {
     //test for logo.svg
-    it('Should render the logo as an SVG', () => {
-        function Logo (shape, textColor, text) {
-            this.shape = shape
-            this.textColor = textColor
-            this.text = text
-            return `
-            <svg version="1.1" width="300px" height="200px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                ${shape}
-                <text x="40%" y="70%" fill="${textColor}" font-size="100%">${text}</text>
-            </svg>`
-        };
-        const myLogo = new Logo("triangle","black","ABC");
-        // Logo.shape = "triangle"
-        // Logo.textColor = "black"
-        // Logo.text = "ABC"
-        const logoStr = `
+    it('Should return the logo as an SVG file', () => {
+        let logo = generateSvg('circle', 'black', 'ABC');
+        let myLogoStr = `
         <svg version="1.1" width="300px" height="200px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <polygon fill="white" points="50 15, 100 100, 0 100" /
-        <text x="40%" y="70%" fill="black" font-size="100%">ABC</text>
+            circle
+            <text x="40%" y="70%" fill="black" font-size="100%">ABC</text>
         </svg>`
 
-        expect(myLogo.render().match(logoStr));
+        expect(logo).toBe(myLogoStr);
     })
 })
