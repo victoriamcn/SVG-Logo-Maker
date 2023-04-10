99e0438a8dde1f6fb0756d33a091cf92e338cad9
Victoria <vmcnorrill@gmail.com>
2023-04-10 18:21:38 -0400

    all tests created and all tests pass

--- tests/generateSvg.test.js
+++ tests/generateSvg.test.js
@@ -1,10 +1,19 @@
-const generateSvg = require('../lib/generateSvg')
+const generateSvg = require('../lib/generateSvg');
+const { Circle } = require('../lib/shapes.js');
 
 describe('generateSvg', () => {
     //test for logo.svg
     it('Should return the logo as an SVG file', () => {
-        let logo = generateSvg('circle', 'black', 'ABC');
-        let myLogoStr = `<svg version="1.1" width="300px" height="200px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle fill="white" cx="50" cy="50" r="50" /><text x="40%" y="70%" fill="black" font-size="100%">ABC</text></svg>`
+        //
+        let shape = new Circle();
+        let shapecolor = "white";
+        shape.set(shapecolor);
+        let textColor = "black";
+        let text = "ABC";
+
+
+        let logo = generateSvg(shape, textColor, text);
+        let myLogoStr = `<svg version="1.1" width="300px" height="200px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">${shape.render()}<text x="40%" y="70%" fill="${textColor}" font-size="100%">${text}</text></svg>`
 
         expect(logo).toBe(myLogoStr);
     })
